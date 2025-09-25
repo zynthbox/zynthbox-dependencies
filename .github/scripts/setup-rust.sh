f513ec1af6fe4f1e8295687b1d7a0e798e182fbf
probal31 <probal31@gmail.com>
2025-09-25 15:51:24 +0530

    Add cargo to path

--- .github/scripts/setup-rust.sh
+++ .github/scripts/setup-rust.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 source $HOME/.cargo/env
+export PATH="$HOME/.cargo/bin:$PATH"
+
 rustc --version
 cargo --version
