a62feb31440f59489dad5b65eef3cb16d02d3bb0
probal31 <probal31@gmail.com>
2025-09-25 15:57:03 +0530

    Add cargo to GITHUB_PATH to make it available for rest of the job

--- .github/scripts/setup-rust.sh
+++ .github/scripts/setup-rust.sh
@@ -2,8 +2,11 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-source $HOME/.cargo/env
-export PATH="$HOME/.cargo/bin:$PATH"
+# Source the Rust environment for this shell session
+source "$HOME/.cargo/env"
+
+# Persist Cargo bin in PATH for all subsequent steps
+echo "$HOME/.cargo/bin" >> "$GITHUB_PATH"
 
 rustc --version
 cargo --version
